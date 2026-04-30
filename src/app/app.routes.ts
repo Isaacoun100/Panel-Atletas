da126523ab26568bb19d0394e690985151701672
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-04-29 23:57:49 -0600

    Routes corrected for the new components
    The routes on the file were for the previous components that are no longer needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,17 @@
 import { Routes } from '@angular/router';
-import { CrearAtleta } from './crear-atleta/crear-atleta';
-import { DashboardAtletas } from './dashboard-atletas/dashboard-atletas';
-import { DashboardAdmin } from './dashboard-admin/dashboard-admin';
 import { IncioSesion } from './incio-sesion/incio-sesion';
-import { VistaAtletas } from './vista-atletas/vista-atletas';
-import { VistaFormulario } from './vista-formulario/vista-formulario';
-import { VistaUsuario } from './vista-usuario/vista-usuario';
+import { Dashboard } from './dashboard/dashboard';
+import { Atletas } from './atletas/atletas';
+import { RegistrarAtleta } from './registrar-atleta/registrar-atleta';
+import { MiPerfil } from './mi-perfil/mi-perfil';
+import { NuevoAtleta } from './nuevo-atleta/nuevo-atleta';
 
 export const routes: Routes = [
-    
   { path: '', redirectTo: 'inicio-sesion', pathMatch: 'full' },
   { path: 'inicio-sesion', component: IncioSesion },
-  { path: 'dashboard-admin', component: DashboardAdmin },
-  { path: 'dashboard-atletas', component: DashboardAtletas },
-  { path: 'crear-atleta', component: CrearAtleta },
-  { path: 'vista-atletas', component: VistaAtletas },
-  { path: 'vista-formulario', component: VistaFormulario },
-  { path: 'vista-usuario', component: VistaUsuario }
-  
+  { path: 'dashboard', component: Dashboard },
+  { path: 'atletas', component: Atletas },
+  { path: 'registrar-atleta', component: RegistrarAtleta },
+  { path: 'mi-perfil', component: MiPerfil },
+  { path: 'nuevo-atleta', component: NuevoAtleta },
 ];
