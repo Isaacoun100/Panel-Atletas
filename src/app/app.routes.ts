a4a1466577a30a2619589b17fc65f05851f16b3b
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2026-05-03 00:56:20 -0600

    Routes updated for the new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,18 @@ import { Routes } from '@angular/router';
 import { IncioSesion } from './incio-sesion/incio-sesion';
 import { Dashboard } from './dashboard/dashboard';
 import { Atletas } from './atletas/atletas';
-import { RegistrarAtleta } from './registrar-atleta/registrar-atleta';
 import { MiPerfil } from './mi-perfil/mi-perfil';
 import { NuevoAtleta } from './nuevo-atleta/nuevo-atleta';
+import { Administrar } from './administrar/administrar';
+import { RegistrarAtleta } from './registrar-atleta/registrar-atleta';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio-sesion', pathMatch: 'full' },
   { path: 'inicio-sesion', component: IncioSesion },
   { path: 'dashboard', component: Dashboard },
   { path: 'atletas', component: Atletas },
-  { path: 'registrar-atleta', component: RegistrarAtleta },
   { path: 'mi-perfil', component: MiPerfil },
   { path: 'nuevo-atleta', component: NuevoAtleta },
+  { path: 'administrar', component: Administrar },
+  { path: 'registrar-atleta', component: RegistrarAtleta },
 ];
